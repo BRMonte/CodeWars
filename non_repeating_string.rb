0b7b03456bf1d1ed23dd2ed8a25654c094841b2c
Bruno Montebello <brunormontebello@hotmail.com>
2024-03-11 23:16:43 -0300

    Move code

--- non_repeating_string.rb
+++
@@ -1,4 +0,0 @@
-# Write a function named first_non_repeating_letter
-# that takes a string input, and returns the first character that is not repeated anywhere in the string.
-# Ex: if given the input 'stress', the function should return 't', since the letter t only occurs once in the string, and occurs first in the string.
-
